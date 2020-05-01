f19a72deb36f61e5019519ea96f35ef047e0cc9f
shisan <1969299278@qq.com>
2020-05-01 09:49:06 +0800

    合并---V1 Paint Circle  V2 Paint Triangle  v3 paint rectangle

---
+++ Paint.java
@@ -0,0 +1,11 @@
+public class Paint{
+   public void circle(){
+   	System.out.pritnln("画圆");
+   }
+   public void triangle(){
+   	System.out.pritnln("画三角形");
+   }
+   public void rectangle(){
+   	System.out.println("画矩形");
+   }
+}
